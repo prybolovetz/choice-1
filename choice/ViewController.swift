5a6213d5417cc885c1f6e03a98a00e01f3df2525
Ivan Prybolovetz <ivan.prybo@gmail.com>
2019-01-11 14:48:56 +0300

    Initial Commit

---
+++ choice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  choice
+//
+//  Created by Ivan on 1/11/19.
+//  Copyright © 2019 Ivan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
